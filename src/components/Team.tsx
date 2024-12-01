b8787a417c9322e499e010b733f1352d610e2b17
Shawn Wilson <shawnwilsontrpt@gmail.com>
2024-12-01 17:36:43 -0500

    pointer events none

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,6 +1,6 @@
 export default function Team() {
   return (
-    <div className="bg-secondary py-24 sm:py-32">
+    <div className="bg-secondary pointer-events-none py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-pretty text-4xl font-semibold tracking-tight text-tertiary sm:text-5xl">Our team</h2>
