01a972d98d80ba4d116de6521256516825657bb2
Shawn Wilson <shawnwilsontrpt@gmail.com>
2024-12-01 16:46:40 -0500

    our values component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
+import OurValues from "@/components/OurValues";
 
-const AboutPage = () => {
+export default function AboutPage() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>Welcome to the About Us page!</p>
-    </div>
+    <>
+      <OurValues />
+    </>
   );
 };
-
-export default AboutPage;
