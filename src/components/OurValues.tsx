c467cce317ac2b2d049ff05310f037156056cf2e
Shawn Wilson <shawnwilsontrpt@gmail.com>
2024-12-01 17:36:43 -0500

    pointer events none

--- src/components/OurValues.tsx
+++ src/components/OurValues.tsx
@@ -2,7 +2,7 @@ import { CogIcon } from "@/images/icons/CogIcon"
 
 export default function OurValues() {
   return (
-    <div className="bg-secondary py-24 sm:py-32">
+    <div className="bg-secondary pointer-events-none py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-pretty text-4xl font-semibold tracking-tight text-tertiary sm:text-5xl">
