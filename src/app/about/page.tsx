a326fa46cba0bef568df900a5cea31af4ae9d678
Shawn Wilson <shawnwilsontrpt@gmail.com>
2024-12-01 16:57:21 -0500

    our team component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import OurValues from "@/components/OurValues";
+import Team from "@/components/Team";
 
 export default function AboutPage() {
   return (
     <>
       <OurValues />
+      <Team />
     </>
   );
 };
